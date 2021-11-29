bc9028dbd4559f17f0cea02e902026ef8ac55202
sjh <277479420@qq.com>
2021-11-29 12:49:34 +0800

    commit

--- cdk-workshop/src/main/java/com/myorg/CdkWorkshopStack.java
+++ cdk-workshop/src/main/java/com/myorg/CdkWorkshopStack.java
@@ -9,21 +9,19 @@ import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
 import software.amazon.awscdk.services.sqs.Queue;
 
 public class CdkWorkshopStack extends Stack {
-    public CdkWorkshopStack(final Construct parent, final String id) {
-        this(parent, id, null);
-    }
+	public CdkWorkshopStack(final Construct parent, final String id) {
+		this(parent, id, null);
+	}
 
-    public CdkWorkshopStack(final Construct parent, final String id, final StackProps props) {
-        super(parent, id, props);
+	public CdkWorkshopStack(final Construct parent, final String id, final StackProps props) {
+		super(parent, id, props);
 
-        final Queue queue = Queue.Builder.create(this, "CdkWorkshopQueue")
-                .visibilityTimeout(Duration.seconds(300))
-                .build();
+		final Queue queue = Queue.Builder.create(this, "CdkWorkshopQueue").visibilityTimeout(Duration.seconds(300))
+				.build();
 
-        final Topic topic = Topic.Builder.create(this, "CdkWorkshopTopic")
-            .displayName("My First Topic Yeah")
-            .build();
+		final Topic topic = Topic.Builder.create(this, "CdkWorkshopTopic").displayName("My First Topic Yeah").build();
 
-        topic.addSubscription(new SqsSubscription(queue));
-    }
+		topic.addSubscription(new SqsSubscription(queue));
+		System.out.print(topic.getTopicName());
+	}
 }
